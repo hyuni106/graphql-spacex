f42fc07e79af619220e3b9e4d704d87d9bd8b00b
hyuni106 <suhyuni106@hanmail.net>
2024-12-05 18:30:11 +0900

    PagerView 추가 및 Launch 상세 이미지 배열 연결

--- src/views/LaunchDetail/index.tsx
+++ src/views/LaunchDetail/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState, useEffect, useMemo} from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  useWindowDimensions,
+  ScaledSize,
+  Text,
+} from 'react-native';
 
 import {ScreenName, Props} from 'views';
 import {Colors} from 'styles';
+import {LaunchesAPI} from 'network/LaunchesAPI';
+import PagerView from 'react-native-pager-view';
+import {Launch} from 'models';
+import {hexAlpha} from 'styles/colors';
 
 export interface LaunchDetailScreenProps {
   launchId: string;
@@ -13,15 +24,84 @@ const LaunchDetailScreen = ({
 }: Props<ScreenName.LaunchDetailScreen>): React.ReactElement => {
   const {launchId} = route.params;
 
-  return <View style={styles.root}></View>;
+  const window = useWindowDimensions();
+  const styles = useMemo(() => createStyles(window), [window]);
+
+  const {data, loading} = LaunchesAPI.useLaunchDetail(launchId);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [launch, setLaunch] = useState<Launch>();
+  const [launchImgList, setLaunchImgList] = useState<string[]>([]);
+  const [pageIndex, setPageIndex] = useState(0);
+
+  useEffect(() => {
+    if (data && data.launch) {
+      setLaunch(data.launch);
+      setLaunchImgList([...data.launch.links.flickr_images]);
+    }
+  }, [data]);
+
+  const renderPagerImageItem = () => {
+    return launchImgList.map((item: string, idx: number) => (
+      <Image
+        key={`${idx}`}
+        style={styles.launchImage}
+        source={{uri: item}}
+        resizeMode="cover"
+      />
+    ));
+  };
+
+  return (
+    <View style={styles.root}>
+      {!loading && launchImgList.length > 0 && (
+        <View style={styles.pagerContainer}>
+          <PagerView
+            style={styles.pagerView}
+            initialPage={pageIndex}
+            collapsable={false}
+            onPageSelected={e => setPageIndex(e.nativeEvent.position)}>
+            {renderPagerImageItem()}
+          </PagerView>
+          <Text style={styles.paginationText}>{`${pageIndex + 1} / ${
+            launchImgList.length
+          }`}</Text>
+        </View>
+      )}
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: Colors.white,
-  },
-});
+const createStyles = (scaledSize: ScaledSize) => {
+  return StyleSheet.create({
+    root: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: Colors.white,
+    },
+    pagerContainer: {
+      width: scaledSize.width,
+      height: 250,
+    },
+    pagerView: {
+      width: '100%',
+      height: '100%',
+    },
+    launchImage: {
+      flex: 1,
+    },
+    paginationText: {
+      position: 'absolute',
+      right: 1,
+      bottom: 1,
+      marginRight: 12,
+      marginBottom: 8,
+      paddingHorizontal: 12,
+      paddingVertical: 4,
+      borderRadius: 16,
+      backgroundColor: hexAlpha(Colors.white, 0.6),
+    },
+  });
+};
 
 export default LaunchDetailScreen;
